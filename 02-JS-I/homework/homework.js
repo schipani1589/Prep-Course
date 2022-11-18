202bbb4c14c921cd56fda7954c72012696e775c5
unknown <juan.schipani@hotmail.com>
2022-11-18 18:37:37 -0300

    prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "cuerda";
 const nuevoNum = 225;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
